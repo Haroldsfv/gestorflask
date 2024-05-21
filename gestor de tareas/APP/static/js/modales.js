3c850a937e6e009c424593433639a8ee7318f8eb
Haroldsfv <hfuqenevargas@gmail.com>
2024-05-21 13:26:23 -0500

    first commit

---
+++ gestor de tareas/APP/static/js/modales.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+$("#btnActualizar").click(function()
+{
+ cargarmodal.load("../templates/Modaltareas.html");
+});
+
+function cargarmodal(){
+    $('#Mtareas').load(ruta,function(){
+        $('#Mtareas').modal("show");
+    });
+};
+
+
+});
+
+
+
